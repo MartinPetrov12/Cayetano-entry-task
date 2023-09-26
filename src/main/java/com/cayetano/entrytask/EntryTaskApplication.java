89efcef78b3b89855d6421a768536eeae7646cee
Martin Petrov <m.a.petrov@student.tudelft.nl>
2023-09-26 11:21:39 +0200

    Initialize project

---
+++ src/main/java/com/cayetano/entrytask/EntryTaskApplication.java
@@ -0,0 +1,13 @@
+package com.cayetano.entrytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cayetano/entrytask/EntryTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cayetano.entrytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
